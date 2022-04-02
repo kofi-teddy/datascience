1b3613ac666bb782f3efc96e5f71a0d8d6715c92
kofi-teddy <agudogot@gmail.com>
2022-04-02 14:45:57 +0000

    merge product and purchase dataframe

--- apps/products/views.py
+++ apps/products/views.py
@@ -7,9 +7,11 @@ from apps.products.models import Product, Purchase
 def chart_select_view(request):
     product_df = pd.DataFrame(Product.objects.all().values())
     purchase_df = pd.DataFrame(Purchase.objects.all().values())
-    print(product_df)
+    product_df['product_id'] = (Purchase.objects.all().values())
+    df = pd.merge(purchase_df, product_df, on='product_id')
     context = {
         'products': product_df.to_html(),
         'purchases': purchase_df.to_html(),
+        'df': df.to_html(),
     }
     return render(request, 'products/main.html', context)
