80817b6c50a4cc24665942d4ed6c821da4251e7b
kofi-teddy <agudogot@gmail.com>
2022-04-02 12:18:57 +0000

    purchase models fixes

--- apps/products/admin.py
+++ apps/products/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from apps.products.models import Product
+from apps.products.models import Product, Purchase
 
 
 admin.site.register(Product)
+admin.site.register(Purchase)
+
--- apps/products/models.py
+++ apps/products/models.py
@@ -10,16 +10,16 @@ class Product(models.Model):
 
 
 class Purchase(models.Model):
-    products = models.ForeignKey(Product, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
     price = models.PositiveIntegerField()
     quantity = models.PositiveIntegerField()
-    total_price = models.PositiveIntegerField()
+    total_price = models.PositiveIntegerField(blank=True)
     salesman = models.ForeignKey(User, on_delete=models.CASCADE)
     date = models.DateTimeField(auto_now_add=True)
 
     def save(self, *args, **kwargs):
         self.total_price = self.price * self.quantity
-        super.save(*args, **kwargs)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f'Sales {self.product.name} - {self.quantity} - {self.price}'
