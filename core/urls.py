bd4f6af2a40437f378d78441bc205e49c3b09d72
kofi-teddy <agudogot@gmail.com>
2022-04-02 14:27:28 +0000

    products templates fix

--- core/urls.py
+++ core/urls.py
@@ -4,7 +4,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('performance', include('apps.products.urls', namespace='products'))
+    path('', include('apps.products.urls', namespace='products'))
 ]
 
 if settings.DEBUG:
