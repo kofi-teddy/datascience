7ca49f5f2567dbdecfce6c8af4b0b1b597af6c6c
kofi-teddy <agudogot@gmail.com>
2022-04-02 11:38:13 +0000

    product

--- apps/products/models.py
+++ apps/products/models.py
@@ -1,2 +1,9 @@
 from django.db import models
 
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.name) 
