87958d350c6d8137a4eb43528d544288522ab531
kofi-teddy <agudogot@gmail.com>
2022-04-02 14:19:29 +0000

    cart_select_view

--- apps/products/urls.py
+++ apps/products/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from .views import chart_select_view
 
 app_name = 'products'
 
-urlpatterns = []
+urlpatterns = [
+    path('', chart_select_view, name="main-products-view"),
+]
