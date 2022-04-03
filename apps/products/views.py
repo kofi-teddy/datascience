72b7f9156a21963d64ec8d9e52260fe47e07cc4f
kofi-teddy <agudogot@gmail.com>
2022-04-03 22:45:07 +0000

    cleaning form

--- apps/products/views.py
+++ apps/products/views.py
@@ -10,6 +10,11 @@ def chart_select_view(request):
     product_df['product_id'] = product_df['id']
     df = pd.merge(purchase_df, product_df, on='product_id').drop(['id_y', 'date_y'], axis=1).rename({'id_x': 'id', 'date_x': 'date'}, axis=1)
     print(df)
+    if request.method == 'POST':
+        print(request.POST)
+        chart_type = request.POST['sales']
+        date_from = request.POST['date_from']
+        date_to = request.POST['date_to']
     context = {
         'products': product_df.to_html(),
         'purchases': purchase_df.to_html(),
