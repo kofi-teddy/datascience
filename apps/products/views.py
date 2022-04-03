72d9c823e307ea5c48f7ef8fcc0e180e9550fefe
kofi-teddy <agudogot@gmail.com>
2022-04-03 22:50:14 +0000

    updated date

--- apps/products/views.py
+++ apps/products/views.py
@@ -9,12 +9,12 @@ def chart_select_view(request):
     purchase_df = pd.DataFrame(Purchase.objects.all().values())
     product_df['product_id'] = product_df['id']
     df = pd.merge(purchase_df, product_df, on='product_id').drop(['id_y', 'date_y'], axis=1).rename({'id_x': 'id', 'date_x': 'date'}, axis=1)
-    print(df)
     if request.method == 'POST':
         print(request.POST)
         chart_type = request.POST['sales']
         date_from = request.POST['date_from']
         date_to = request.POST['date_to']
+        print(chart_type, date_from, date_to)
     context = {
         'products': product_df.to_html(),
         'purchases': purchase_df.to_html(),
