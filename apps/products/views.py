417207be0793365d7552b52e86cbfb6662dd1694
kofi-teddy <agudogot@gmail.com>
2022-04-03 22:57:27 +0000

    added error message

--- apps/products/views.py
+++ apps/products/views.py
@@ -5,17 +5,22 @@ from apps.products.models import Product, Purchase
 
 
 def chart_select_view(request):
+    error_message = None
+
     product_df = pd.DataFrame(Product.objects.all().values())
     purchase_df = pd.DataFrame(Purchase.objects.all().values())
     product_df['product_id'] = product_df['id']
-    df = pd.merge(purchase_df, product_df, on='product_id').drop(['id_y', 'date_y'], axis=1).rename({'id_x': 'id', 'date_x': 'date'}, axis=1)
-    if request.method == 'POST':
-        print(request.POST)
-        chart_type = request.POST['sales']
-        date_from = request.POST['date_from']
-        date_to = request.POST['date_to']
-        print(chart_type, date_from, date_to)
+    if purchase_df.shape[0] > 0:
+        df = pd.merge(purchase_df, product_df, on='product_id').drop(['id_y', 'date_y'], axis=1).rename({'id_x': 'id', 'date_x': 'date'}, axis=1)
+        if request.method == 'POST':
+            chart_type = request.POST['sales']
+            date_from = request.POST['date_from']
+            date_to = request.POST['date_to']
+    else:
+        error_message = 'No records in the database'
+
     context = {
+        'error_message': error_message,
         'products': product_df.to_html(),
         'purchases': purchase_df.to_html(),
         'df': df.to_html(),
