1ecd8c629ae551196f35ff40e3707de2aa7a970b
kofi-teddy <agudogot@gmail.com>
2022-04-03 15:45:48 +0000

    drop tables

--- apps/products/views.py
+++ apps/products/views.py
@@ -7,8 +7,9 @@ from apps.products.models import Product, Purchase
 def chart_select_view(request):
     product_df = pd.DataFrame(Product.objects.all().values())
     purchase_df = pd.DataFrame(Purchase.objects.all().values())
-    product_df['product_id'] = (Purchase.objects.all().values())
-    df = pd.merge(purchase_df, product_df, on='product_id')
+    product_df['product_id'] = product_df['id']
+    df = pd.merge(purchase_df, product_df, on='product_id').drop(['id_y', 'date_y'], axis=1).rename({'id_x': 'id', 'date_x': 'date'}, axis=1)
+    print(df)
     context = {
         'products': product_df.to_html(),
         'purchases': purchase_df.to_html(),
